777b5902b32646545271b91e744b058fdbf39243
Andrii Charkivskyi <q#@>
2023-05-08 13:54:06 +0300

    ServiceResult fix.

--- ChatApp.BLL/Helpers/ServiceResult.cs
+++ ChatApp.BLL/Helpers/ServiceResult.cs
@@ -2,12 +2,11 @@ namespace ChatApp.BLL.Helpers;
 
 public class ServiceResult
 {
-    public bool Succeeded { get; private set; }
-    public List<KeyValuePair<string, string>>? Errors { get; private set; }
+    public bool Succeeded => Errors == null;
+    public List<KeyValuePair<string, string>>? Errors { get; }
     
     public ServiceResult( List<KeyValuePair<string, string>>? errors = null)
     {
-        Succeeded = errors == null;
         Errors = errors;
     }
 }
@@ -16,17 +15,15 @@ public class ServiceResult<T>
 {
     public ServiceResult(List<KeyValuePair<string, string>>? errors)
     {
-        Succeeded = false;
         Errors = errors;
     }
 
     public ServiceResult(T value)
     {
-        Succeeded = true;
         Value = value;
     }
 
-    public bool Succeeded { get; private set; }
+    public bool Succeeded => Value != null;
     public List<KeyValuePair<string, string>>? Errors { get; private set; }
-    public T? Value { get; private set; }
+    public T? Value { get; }
 }
